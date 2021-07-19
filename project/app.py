9db8b25891ed5f23f870ca2f9b9b55a74958ec21
Ray West <rbrandonwest@gmail.com>
2021-07-19 11:45:13 -0400

    m

--- project/app.py
+++ project/app.py
@@ -13,19 +13,16 @@ def index():
     return render_template('index.html')
 
 
-def gen_frames(camera):
-    while True:
-        success, image = camera.read()  # read the camera frame
-        ret, jpeg = cv2.imencode('.jpg', image)
-        frame = jpeg.tobytes()
-        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-        # if not success:
-        #     break
-        # else:
-        #     ret, buffer = cv2.imencode('.jpg', frame)
-        #     frame = buffer.tobytes()
-        #     # concat frame one by one and show result
-        #     yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+def gen_frames():
+    while camera.isOpened():
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 
 @main.route('/stream')
@@ -37,16 +34,3 @@ def stream():
 @main.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-# def gen_frames():
-#     camera = cv2.VideoCapture(-1)
-#     while camera.isOpened():
-#         success, frame = camera.read()  # read the camera frame
-#         if not success:
-#             break
-#         else:
-#             ret, buffer = cv2.imencode('.jpg', frame)
-#             frame = buffer.tobytes()
-#             yield (b'--frame\r\n'
-#                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result

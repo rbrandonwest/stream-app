3b21dde73f657616f7a4652c10695dad396e85e5
Ray West <rbrandonwest@gmail.com>
2021-07-19 10:50:44 -0400

    m

--- project/app.py
+++ project/app.py
@@ -8,21 +8,24 @@ main = Blueprint('main', __name__)
 camera = cv2.VideoCapture(0)
 
 
-def gen_frames(camera):
-    while True:
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            # concat frame one by one and show result
-            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+def gen_frames(camera):
+    while True:
+        success, image = camera.read()  # read the camera frame
+        ret, jpeg = cv2.imencode('.jpg', image)
+        frame = jpeg.tobytes()
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        # if not success:
+        #     break
+        # else:
+        #     ret, buffer = cv2.imencode('.jpg', frame)
+        #     frame = buffer.tobytes()
+        #     # concat frame one by one and show result
+        #     yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @main.route('/stream')

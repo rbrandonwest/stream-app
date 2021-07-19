2f249a10d823381c01d2b25bf6f1702ae3131225
Ray West <rbrandonwest@gmail.com>
2021-07-19 11:09:14 -0400

    m

--- project/app.py
+++ project/app.py
@@ -39,14 +39,14 @@ def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-def gen_frames():
-    camera = cv2.VideoCapture(-1)
-    while camera.isOpened():
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+# def gen_frames():
+#     camera = cv2.VideoCapture(-1)
+#     while camera.isOpened():
+#         success, frame = camera.read()  # read the camera frame
+#         if not success:
+#             break
+#         else:
+#             ret, buffer = cv2.imencode('.jpg', frame)
+#             frame = buffer.tobytes()
+#             yield (b'--frame\r\n'
+#                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result

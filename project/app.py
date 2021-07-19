f4a3e1ee107c1d88bdbaa6d77faf0fb0fbdee250
Ray West <rbrandonwest@gmail.com>
2021-07-18 22:58:01 -0400

    added video

--- project/app.py
+++ project/app.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, Response
 from flask_login import login_required, current_user
 from . import db
+import cv2
 
 main = Blueprint('main', __name__)
+
+camera = cv2.VideoCapture(0)
 
 
 @main.route('/')
@@ -10,7 +13,24 @@ def index():
     return render_template('index.html')
 
 
+# @main.route('/stream')
+# @login_required
+# def profile():
+#     return render_template('stream.html', name=current_user.name)
+
 @main.route('/stream')
 @login_required
-def profile():
-    return render_template('stream.html', name=current_user.name)
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
